a81f65a3bc60e9546d77ad6bd20aa2550bb0dcce
Denis Lapuente <darthsuicune@gmail.com>
2014-03-12 11:36:45 +0100

    Add sign up

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Denis"
+    surname  "Lapuente"
+    email    "denis@localhost.so"
+    password "suicune"
+    password_confirmation "suicune"
+  end
+end
